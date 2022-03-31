2b6091c614ef132f13a8d67a96512cb995404c9d
Sunil Kumar Yadav <86093794+SUNILmobcoder@users.noreply.github.com>
2022-03-31 12:10:46 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # node-zip-multiple-file
+
+# How to use
+
+1. Clone the repo
+2. npm install
+3. node index.js or nodemon index.js
+
+## What it contains
+
+1. Two routes
+   1. "http://localhost:3000//" => you will see the message "Hello there"
+   2. "http://localhost:3000//zip" => It should download the zip file containing 2 imgs and 2 videos
+2. Lib used
+   1. express
+   2. express-zip
---
+++ index.js
@@ -0,0 +1,18 @@
+const { append } = require("express/lib/response");
+const zip = require("express-zip");
+
+const App = require("express")();
+
+App.get("/", (req, res) => {
+  res.send("Hello there");
+});
+
+App.get("/zip", (req, res) => {
+  res.zip([
+    { path: "./res/1.jpg", name: "./res/1.jpg" },
+    { path: "./res/2.jpg", name: "./res/2.jpg" },
+    { path: "./res/1.mp4", name: "./res/1.mp4" },
+    { path: "./res/2.mp4", name: "./res/2.mp4" },
+  ]);
+});
+App.listen(3000);
